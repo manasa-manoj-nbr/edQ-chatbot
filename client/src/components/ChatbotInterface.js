450052e19da4a5005a38add2b9d4b74a02a4ad5b
manasa-manoj-nbr <manasa23bcy41@iiitkottayam.ac.in>
2025-06-21 00:27:40 +0530

    n8n production

--- client/src/components/ChatbotInterface.js
+++ client/src/components/ChatbotInterface.js
@@ -63,7 +63,7 @@ const userId = params.get("userid");
   setIsLoading(true);
 
      try {
-    const response = await axios.post(`https://edquery-chat.app.n8n.cloud/webhook-test/chatbot`, {
+    const response = await axios.post(`https://edquery-chat.app.n8n.cloud/webhook/chatbot`, {
         question: inputMessage,
         userid: userId
     }, {
