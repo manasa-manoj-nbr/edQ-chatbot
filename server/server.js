326fcb7b110e24b8aada97388515d59704cf3fc6
manasa-manoj-nbr <manasa23bcy41@iiitkottayam.ac.in>
2025-06-24 23:23:57 +0530

    update

--- server/server.js
+++ server/server.js
@@ -83,6 +83,6 @@ async function main() {
 
 main(); // Call the function
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
